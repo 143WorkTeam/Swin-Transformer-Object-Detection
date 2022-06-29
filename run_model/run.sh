8ca4d958317d3a86cde872fbe409a366d6424771
Microsoft <1033718596@qq.com>
2022-06-29 13:07:21 -0700

    succeed in test

--- run_model/run.sh
+++ run_model/run.sh
@@ -17,14 +17,14 @@ python tools/test.py\
  configs/swin/cascade_mask_rcnn_swin_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py \
  checkpoints/cascade_mask_rcnn_swin_base_patch4_window7.pth \
  --show \
- --eval= bbox segm
+ --eval bbox segm
 # train on smaller files for better debug
 python -m exps.make_smaller_coco
 
 python tools/train.py \
-    configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py \
+    configs/faster_rcnn/cascade_mask_rcnn_swin_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py \
     --auto-scale-lr \
-    --cfg-options auto_scale_lr.base_batch_size=12 \
+    --cfg-options auto_scale_lr.base_batch_size=8 \
                     data.workers_per_gpu=8 \
                     data.samples_per_gpu=12 \
                     log_config.interval=100 \
