5f5a9f96a39214044e4b093a0281375dfcf2c452
Microsoft <1033718596@qq.com>
2022-06-29 11:41:42 -0700

    try parameters

--- run_model/run.sh
+++ run_model/run.sh
@@ -2,12 +2,20 @@
 export MMDET_DATASETS=/mnt/coco/
 export OMP_NUM_THREADS=8
 
+mkdir checkpoints
+cd checkpoints
+wget https://github.91chi.fun/https://github.com/SwinTransformer/storage/releases/download/v1.0.2/cascade_mask_rcnn_swin_base_patch4_window7.pth
+wget https://github.91chi.fun/https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224_22k.pth
+# OR
+wget https://github.com/SwinTransformer/storage/releases/download/v1.0.2/cascade_mask_rcnn_swin_base_patch4_window7.pth
+wget https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224_22k.pth
+
 # Test on existing model and checkpoint
 # refer for the website https://github.com/open-mmlab/mmdetection/blob/master/docs/en/1_exist_data_model.md
 # weightes are downloaded from https://github.com/open-mmlab/mmdetection/tree/master/configs/faster_rcnn and save into checkpoints
 python tools/test.py\
- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py \
- checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth \
+ configs/swin/cascade_mask_rcnn_swin_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py \
+ checkpoints/cascade_mask_rcnn_swin_base_patch4_window7.pth \
  --show \
  --work-dir=results \
  --eval="bbox" \
